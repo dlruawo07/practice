6ef37e65558cab774a97c677ef8032a303ecc965
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-05 21:43:31 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.17 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,23 @@
+function convertToDays(date) {
+  var [year, month, day] = date.split(".").map(Number);
+  return year * 336 + month * 28 + day;
+}
+
+function solution(today, terms, privacies) {
+  var answer = [];
+  // 오늘 날짜
+  var todayCount = convertToDays(today);
+  for (var i = 0; i < privacies.length; i++) {
+    var privacy = privacies[i].split(" ");
+    var date = convertToDays(privacy[0]);
+    for (var j = 0; j < terms.length; j++) {
+      var term = terms[j].split(" ");
+      if (term[0] === privacy[1]) {
+        if (todayCount - date >= Number(term[1] * 28)) {
+          answer.push(i + 1);
+        }
+      }
+    }
+  }
+  return answer;
+}
