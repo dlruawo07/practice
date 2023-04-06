2778e483115b9f410ec9f4a9c9c47c7e80ffb9c4
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-06 22:55:19 +0900

    refactored

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,9 +1,11 @@
 function solution(food) {
-    var answer = '';
-    let temp = '';
-    for (let i = 1; i < food.length; i++) {
-        answer += String(i).repeat(Math.floor(food[i] / 2))
-        temp += String(food.length - i).repeat(Math.floor(food[food.length - i]) / 2)
-    }
-    return answer+0+temp;
+  var answer = "";
+  let temp = "";
+  for (let i = 1; i < food.length; i++) {
+    answer += String(i).repeat(Math.floor(food[i] / 2));
+    temp += String(food.length - i).repeat(
+      Math.floor(food[food.length - i]) / 2
+    );
+  }
+  return answer + 0 + temp;
 }
