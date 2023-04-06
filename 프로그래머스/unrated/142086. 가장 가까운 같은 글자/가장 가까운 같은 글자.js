d11bc589809fc1badc72e27f7e9e1644a641fd4d
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-06 11:57:17 +0900

    refactored code

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,13 +2,8 @@ function solution(s) {
     var answer = [];
     answer.push(-1);
     for (let i = 1; i < s.length; i++) {
-        // slice의 indexOf 해서 있으면 현재 index - indexOf
-        var sliceS = s.slice(0, i);
-        var index = sliceS.lastIndexOf(s[i]);
-        if (index !== -1) {
-            index = i - index;
-        }
-        answer.push(index);
+        var index = s.slice(0, i).lastIndexOf(s[i]);
+        (index !== -1) ? answer.push(i - index) : answer.push(index);
     }
     return answer;
 }
