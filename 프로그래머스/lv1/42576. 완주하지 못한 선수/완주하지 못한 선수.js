243cca11e44064948cd9a90b2eb374342310e41e
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-07 12:04:39 +0900

    refactored

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,15 @@
 function solution(participant, completion) {
-  let answer = '';
-  const hashMap = {};
-  for (let comp of completion) {
-      if (hashMap[comp]) {
-          hashMap[comp] += 1
-      } else {
-          hashMap[comp] = 1
-      }
-  }
-
-  for (let part of participant) {
-    if (!hashMap[part]) answer = part;
-    hashMap[part]--;
-  }
-
-  return answer;
+    let answer = '';
+    const hashMap = {};
+    for (let comp of completion) {
+        if (hashMap[comp]) hashMap[comp] += 1
+        else hashMap[comp] = 1
+    }
+    
+    for (let part of participant) {
+        if (!hashMap[part]) answer = part;
+        hashMap[part]--;
+    }
+    
+    return answer;
 }
