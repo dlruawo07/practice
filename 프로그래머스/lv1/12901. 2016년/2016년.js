4379513dfc4357976c36b64e0c8e400eb0373165
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-07 11:13:20 +0900

    [level 1] Title: 2016년, Time: 2.28 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,18 @@
+function solution(a, b) {
+  let days = b;
+  for (let month = 2; month <= a; month++) {
+    if (month == 3) days += 29;
+    else if (month == 5 || month == 7 || month == 10 || month == 12) days += 30;
+    else days += 31;
+  }
+
+  console.log(days);
+
+  if (days % 7 == 0) return "THU";
+  if (days % 7 == 1) return "FRI";
+  if (days % 7 == 2) return "SAT";
+  if (days % 7 == 3) return "SUN";
+  if (days % 7 == 4) return "MON";
+  if (days % 7 == 5) return "TUE";
+  return "WED";
+}
