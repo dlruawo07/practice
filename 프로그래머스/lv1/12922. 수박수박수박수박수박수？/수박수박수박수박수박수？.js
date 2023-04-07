7acb85fca674978f0cb44b81133ebc34df1e5158
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-07 11:30:33 +0900

    added another solution

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,16 @@
+// solution 1
 function solution(n) {
     var answer = '';
     let arr = ["수", "박"]
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++)
         answer += (i % 2 == 0) ? arr[0] : arr[1];
-    }
+    return answer;
+}
+
+// solution 2
+function solution(n) {
+    var answer = '';
+    for (let i = 0; i < Math.floor(n / 2); i++) answer += "수박";
+    if (n % 2 !== 0) answer += "수";
     return answer;
 }
