0590debca179188c70c8708d6460eb4629f75d04
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-07 11:58:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.11 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,18 @@
+function solution(participant, completion) {
+  let answer = '';
+  const hashMap = {};
+  for (let comp of completion) {
+      if (hashMap[comp]) {
+          hashMap[comp] += 1
+      } else {
+          hashMap[comp] = 1
+      }
+  }
+
+  for (let part of participant) {
+    if (!hashMap[part]) answer = part;
+    hashMap[part]--;
+  }
+
+  return answer;
+}
