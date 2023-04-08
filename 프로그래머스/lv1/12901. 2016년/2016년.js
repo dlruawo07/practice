cab698878bd4aef706e4c12ee78bdb04269298d2
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-08 10:26:04 +0900

    added another solution

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,22 +1,15 @@
 function solution(a, b) {
   let days = b;
-  for (let month = 2; month <= a; month++) {
-    if (month == 3) days += 29;
-    else if (month == 5 || month == 7 || month == 10 || month == 12) days += 30;
-    else days += 31;
-  }
-    
-//     let arr = ["THU", "FRI", "SAT", "SUN", "MON", "TUE"];
-    
-//     let remainder = Math.floor(days % 7);
-    
-//     return arr[remainder];
+  let monthdays = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  for (let i = 2; i <= a; i++) days += monthdays[i - 1];
+  let weekdays = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+  return weekdays[days % 7];
+}
 
-  if (days % 7 == 0) return "THU";
-  if (days % 7 == 1) return "FRI";
-  if (days % 7 == 2) return "SAT";
-  if (days % 7 == 3) return "SUN";
-  if (days % 7 == 4) return "MON";
-  if (days % 7 == 5) return "TUE";
-  return "WED";
+function solution(a, b) {
+  const yoil = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let month = String(a).length == 1 ? "0" + String(a) : String(a);
+  let days = String(b).length == 1 ? "0" + String(b) : String(b);
+  let date = new Date(`2016-${month}-${days}`);
+  return yoil[date.getDay()];
 }
