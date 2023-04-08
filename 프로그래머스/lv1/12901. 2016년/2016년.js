55b263711db0cac2787847072c888f4be815a38b
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-08 10:00:16 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -5,6 +5,12 @@ function solution(a, b) {
     else if (month == 5 || month == 7 || month == 10 || month == 12) days += 30;
     else days += 31;
   }
+    
+//     let arr = ["THU", "FRI", "SAT", "SUN", "MON", "TUE"];
+    
+//     let remainder = Math.floor(days % 7);
+    
+//     return arr[remainder];
 
   if (days % 7 == 0) return "THU";
   if (days % 7 == 1) return "FRI";
