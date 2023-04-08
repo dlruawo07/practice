1b576a13379faf289d3fd454ed49a2f0b7821ee4
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-08 15:42:41 +0900

    refactored

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,6 +1,3 @@
 function solution(n) {
-    let squareRoot = Math.sqrt(n);
-    return (squareRoot === parseInt(squareRoot)) ? Math.pow(squareRoot + 1, 2) : -1;
-    // let sqrt = Math.floor(Math.sqrt(n));
-    // return (sqrtInt == Math.sqrt(n))) ? Math.pow(sqrtInt + 1, 2)) : -1;
+    return Math.sqrt(n) === parseInt(Math.sqrt(n)) ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
 }
