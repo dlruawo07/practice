4cc261ff6dd4059e0b45b59b3c0361db466afa15
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-10 22:51:23 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,12 @@
+function solution(new_id) {
+  let answer = new_id.toLowerCase();
+  answer = answer.replace(/[^a-z0-9-_.]/g, "");
+  answer = answer.replace(/[.]{2,}/g, ".");
+  answer = answer.replace(/^\.|\.$/g, "");
+  if (answer.length === 0) answer = "a";
+  if (answer.length >= 16) answer = answer.slice(0, 15).replace(/\.$/, "");
+  let len = answer.length;
+  if (len <= 2) answer = answer + answer[len - 1].repeat(3 - len);
+
+  return answer;
+}
