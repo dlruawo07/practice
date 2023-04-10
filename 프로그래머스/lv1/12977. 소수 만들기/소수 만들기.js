5af5d2eed59af08e8ff665e10db7f554facda5f5
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-10 15:27:48 +0900

    refactored

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,5 +1,5 @@
 function isPrime(x) {
-  for (let i = 2; i < x; i++) {
+  for (let i = 2; i <= Math.ceil(Math.sqrt(x)); i++) {
     if (x % i === 0) return false;
   }
   return true;
@@ -8,13 +8,12 @@ function isPrime(x) {
 function solution(nums) {
   let answer = 0;
   let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
       for (let k = j + 1; k < nums.length; k++) {
         if (isPrime(nums[i] + nums[j] + nums[k])) answer++;
       }
     }
   }
-
   return answer;
 }
