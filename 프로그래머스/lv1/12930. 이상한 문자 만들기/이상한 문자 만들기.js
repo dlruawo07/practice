03e3d5a2b480af3036723f8cd9c9eea11a144199
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-10 17:52:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,12 +1,11 @@
 function solution(s) {
-  var answer = "";
-  const sArr = s.split(" ");
-  for (let i = 0; i < sArr.length; i++) {
-    if (i !== 0) answer += " ";
-    for (let j = 0; j < sArr[i].length; j++) {
-      if (j % 2 === 0) answer += sArr[i][j].toUpperCase();
-      else if (j % 2 !== 0) answer += sArr[i][j].toLowerCase();
+    let answer = "";
+    let arr = s.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== 0) answer += " ";
+        for (let j = 0; j < arr[i].length; j++) {
+            answer += (j % 2 === 0) ? arr[i][j].toUpperCase() : arr[i][j].toLowerCase();
+        }
     }
-  }
-  return answer;
+    return answer;
 }
