4b1a5a37953980cf6f0d47f626f71c986b08514f
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-10 10:50:10 +0900

    added another solution

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -3,23 +3,27 @@ function solution(lottos, win_nums) {
   let min = 0;
   let max = 0;
   for (let i = 0; i < lottos.length; i++) {
-    if (win_nums.includes(lottos[i])) {
-      min++;
-      max++;
-    } else if (lottos[i] === 0) {
-      max++;
-    }
+    if (win_nums.includes(lottos[i])) min++;
+    else if (lottos[i] === 0) max++;
   }
+  max += min;
   let rank = [6, 5, 4, 3, 2];
-  if (rank.indexOf(max) === -1) {
-    answer.push(6);
-  } else {
-    answer.push(rank.indexOf(max) + 1);
-  }
-  if (rank.indexOf(min) === -1) {
-    answer.push(6);
-  } else {
-    answer.push(rank.indexOf(min) + 1);
-  }
+  if (rank.indexOf(max) === -1) answer.push(6);
+  else answer.push(rank.indexOf(max) + 1);
+  if (rank.indexOf(min) === -1) answer.push(6);
+  else answer.push(rank.indexOf(min) + 1);
   return answer;
+}
+
+function solution(lottos, win_nums) {
+  let min = lottos.filter((ele) => win_nums.includes(ele)).length;
+  let max = lottos.filter((ele) => ele === 0).length + min;
+
+  let rank = [6, 5, 4, 3, 2];
+
+  let maxIndex = rank.indexOf(max);
+  let minIndex = rank.indexOf(min);
+
+  if (maxIndex === -1) return [6, 6];
+  return minIndex === -1 ? [maxIndex + 1, 6] : [maxIndex + 1, minIndex + 1];
 }
