474be34ab3e68f59554128e3fb81db95248eeaa2
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-11 09:43:37 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    var answer = d.sort((a, b) => b - a);
+    answer.filter((ele) => { ele < d; })
+    
+    let sum = answer.reduce((acc, cur) => acc + cur, 0);
+    while (sum > budget) {
+        answer.splice(0, 1);
+        sum = answer.reduce((acc, cur) => acc + cur, 0);
+    }
+    return answer.length;
+}
