812301ac77306e7845e076f477f3ce331a0e144a
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-11 16:58:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.54 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,23 @@
+function solution(board, moves) {
+  var answer = 0;
+  let basket = [];
+
+  for (let move of moves) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][move - 1] !== 0) {
+        let target = board[i][move - 1];
+        board[i][move - 1] = 0;
+        if (basket[basket.length - 1] === target) {
+          basket.pop();
+          answer += 2;
+          break;
+        } else {
+          basket.push(target);
+          break;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
