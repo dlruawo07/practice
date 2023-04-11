527d41203d86fcddf7d62078cf134aa4b0a7c88f
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-11 16:41:32 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,22 @@
+function solution(dartResult) {
+  let s = dartResult;
+  let nPos = 0;
+  let score = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "S") score.push(Number(s.substring(nPos, i)));
+    else if (s[i] === "D")
+      score.push(Math.pow(Number(s.substring(nPos, i)), 2));
+    else if (s[i] === "T")
+      score.push(Math.pow(Number(s.substring(nPos, i)), 3));
+    else if (s[i] === "*") {
+      score[score.length - 1] *= 2;
+      score[score.length - 2] *= 2;
+
+    } else if (s[i] === "#") score[score.length - 1] *= -1;
+    else continue;
+    nPos = i + 1;
+  }
+
+  return score.reduce((acc, cur) => acc + cur, 0);
+}
