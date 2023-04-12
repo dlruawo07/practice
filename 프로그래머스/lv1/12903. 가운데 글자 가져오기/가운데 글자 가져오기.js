e43652715cc054b9a2467d89e6f9f5174dfdec4b
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 11:32:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,3 +1,3 @@
 function solution(s) {
-    return (s.length % 2 === 0) ? s.substr(s.length / 2 - 1, 2) : s[Math.floor(s.length / 2)];
+    return (s.length % 2 === 0) ? s.substring(s.length / 2 - 1, s.length / 2 + 1) : s[Math.floor(s.length / 2)];
 }
