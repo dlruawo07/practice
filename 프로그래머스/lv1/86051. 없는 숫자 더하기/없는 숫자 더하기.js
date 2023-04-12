528ccf95e5e945ccfaf40c651516c6e89d0c9576
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 11:51:49 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,6 +1,13 @@
+Set.prototype.difference = function(otherSet) {
+    const differenceSet = new Set();
+    for (let elem of this) {
+        if (!otherSet.has(elem)) differenceSet.add(elem);
+    }
+    return differenceSet;
+}
+
 function solution(numbers) {
-    var answer = 0;
-    for (let i = 0; i <= 9; i++)
-        if (numbers.indexOf(i) === -1) answer += i;
-    return answer;
+    const numSet = new Set(numbers);
+    const missingNums = new Set(Array.from({length: 10}, (_, i) => i)).difference(numSet);
+    return [...missingNums].reduce((acc, cur) => acc + cur, 0);
 }
