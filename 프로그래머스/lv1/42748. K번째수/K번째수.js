ac0133153ddd19ab7ba9cbd13b904a9a81160bf9
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 16:43:24 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,10 +1,5 @@
 function solution(array, commands) {
-    let answer = [];
-    for (let cmd of commands) {
-        answer.push(
-            array.slice(cmd[0] - 1, cmd[1])
-                .sort((a, b) => a - b)[cmd[2] - 1]
-        );
-    }
-    return answer;
+    return commands.map(([from, to, index]) =>
+        array.slice(from - 1, to).sort((a, b) => a - b)[index - 1]
+    );
 }
