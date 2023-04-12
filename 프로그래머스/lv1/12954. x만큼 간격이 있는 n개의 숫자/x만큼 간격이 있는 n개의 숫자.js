4815eed07822c0c7b00c26f28afa4f23afaf6538
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:11:51 +0900

    added comment

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,3 +1,14 @@
 function solution(x, n) {
     return Array.from({length: n}, (_, i) => x * (i + 1));
 }
+
+// 기존 코드
+// function solution(x, n) {
+//     var answer = [];
+//     for (let i = 0; i < n; i++) answer.push(x + (x * i));
+//     return answer;
+// }
+
+// 지피티가 추천해준 방법이 훨씬 간결하다.
+// Array.from() 메소드로 n개만큼의 배열을 만들어서
+// 배열의 요소를 x부터 시작해 x씩 증가하며 할당했다.
