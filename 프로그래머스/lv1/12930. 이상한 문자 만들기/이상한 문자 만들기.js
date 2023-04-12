95029912e4a96ec46fef58c384d6ed46df9673ad
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:31:46 +0900

    added comment

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -11,3 +11,18 @@ function solution(s) {
     )
     .join(" ");
 }
+
+// 기존 코드
+// function solution(s) {
+//     let answer = "";
+//     let arr = s.split(" ");
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i !== 0) answer += " ";
+//         for (let j = 0; j < arr[i].length; j++) {
+//             answer += (j % 2 === 0) ? arr[i][j].toUpperCase() : arr[i][j].toLowerCase();
+//         }
+//     }
+//     return answer;
+// }
+
+// 각 단어별로 짝/홀수 인덱스를 판단해 대/소문자로 변환 후 join 후 단어들을 전부 join.
