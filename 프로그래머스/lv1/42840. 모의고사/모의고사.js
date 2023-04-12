2c56cc2df9b007011d6759b93442efa6ad55ee12
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 16:02:51 +0900

    [level 1] Title: 모의고사, Time: 1.80 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,20 +1,30 @@
 function solution(answers) {
   var answer = [];
 
-  let arr1 = [1, 2, 3, 4, 5];
-  let arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  let arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const firstArr = [1, 2, 3, 4, 5];
+  const secondArr = [2, 1, 2, 3, 2, 4, 2, 5];
+  const thirdArr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  let ans = [0, 0, 0];
+  let correct = [0, 0, 0];
 
-  for (let i = 0; i < answers.length; i++) {
-    ans[0] += answers[i] === arr1[i % arr1.length] ? 1 : 0;
-    ans[1] += answers[i] === arr2[i % arr2.length] ? 1 : 0;
-    ans[2] += answers[i] === arr3[i % arr3.length] ? 1 : 0;
-  }
-
-  for (let i = 0; i < ans.length; i++)
-    if (ans[i] === Math.max(...ans)) answer.push(i + 1);
-
+  let firstIndex = 0;
+  let secondIndex = 0;
+  let thirdIndex = 0;
+    
+  answers.forEach((answer, index) => {
+    if (answer === firstArr[firstIndex]) correct[0]++;
+    if (answer === secondArr[secondIndex]) correct[1]++;
+    if (answer === thirdArr[thirdIndex]) correct[2]++;
+      
+    firstIndex = (firstIndex + 1) % firstArr.length;
+    secondIndex = (secondIndex + 1) % secondArr.length;
+    thirdIndex = (thirdIndex + 1) % thirdArr.length;
+  });
+    
+  const max = Math.max(...correct);
+  correct.forEach((score, i) => {
+    if (score === max) answer.push(i + 1);              
+  })
+  
   return answer;
 }
