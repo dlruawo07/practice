8d23fdb5635a52268056c1e78837524947213b5f
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:15:09 +0900

    added comment

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -3,3 +3,13 @@ function solution(price, money, count) {
     const diff = sum - money;
     return (diff > 0) ? diff : 0;
 }
+
+// 기존 코드
+// function solution(price, money, count) {
+//     let sum = 0;
+//     for (let i = 1; i <= count; i++) sum += price * i;
+//     return (sum > money) ? sum - money : 0;
+// }
+
+// 지피티 추천 코드는 for문을 사용하는 것이 아니라
+// 등차수열의 합 공식을 사용했기 때문에 더 빠르다.
