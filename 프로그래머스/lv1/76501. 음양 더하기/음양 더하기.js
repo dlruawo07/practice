fa8349c7681e00e4ab32b72b20a2b13e5fa7e7e7
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:03:58 +0900

    added comment

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,3 +1,19 @@
 function solution(absolutes, signs) {
     return absolutes.reduce((acc, cur, i) => acc + (signs[i] ? cur : -cur), 0);
 }
+
+// 기존 코드
+// function solution(absolutes, signs) {
+//     var answer = 0;
+    
+//     for (let i = 0; i < absolutes.length; i++) {
+//         if (signs[i] === true) answer += absolutes[i];
+//         if (signs[i] === false) answer -= absolutes[i];
+//     }
+    
+//     return answer;
+// }
+
+// 두 방법 모두 각각 for와 reduce를 사용했기 때문에 시간복잡도는 같지만
+// reduce를 사용한 방법이 더 간결하고 변수 선언과 초기화가 없기 때문에
+// 첫 번째 방법이 메모리 사용량(공간복잡도) 면에서는 더 효율적이다.
