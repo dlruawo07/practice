ea2263c9f33230e3331dff06ae2d125452b9c50b
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 11:30:34 +0900

    added comment

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,3 +3,5 @@ process.stdin.on('data', data => {
     const [n, m] = data.split(" ");
     console.log("*".repeat(n).concat("\n").repeat(m));
 });
+
+// 원래는 for문을 돌면서 *만 출력했었는데 for문 없이 repeat로 매번 개행을 찍어주는 로직을 지피티가 추천해줬다.
