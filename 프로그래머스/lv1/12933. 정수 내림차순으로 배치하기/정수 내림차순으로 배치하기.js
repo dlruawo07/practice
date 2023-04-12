6e554cd08798f1903cb1ef381eb3616a78090650
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:40:38 +0900

    added comment

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -6,3 +6,11 @@ function solution(n) {
   }
   return Number(digits.sort((a, b) => b - a).join(''));
 }
+
+// 기존 코드
+// function solution(n) {
+//     return Number(String(n).split("").sort((a, b) => b - a).join(""));
+// }
+
+// 각 자릿수를 배열에 담고 난 후 sort vs. 문자열로 바꿔서 sort
+// 타입 변환이 첫 번째가 더 적어서 더 효율적이다.
