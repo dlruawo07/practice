02541303719799542f229b8a0336873fa1c128cc
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:16:43 +0900

    [level 1] Title: 2016년, Time: 0.12 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,15 +1,6 @@
 function solution(a, b) {
-  let days = b;
-  let monthdays = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  for (let i = 2; i <= a; i++) days += monthdays[i - 1];
-  let weekdays = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
-  return weekdays[days % 7];
-}
+  const dayOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-function solution(a, b) {
-  const yoil = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  let month = String(a).length == 1 ? "0" + String(a) : String(a);
-  let days = String(b).length == 1 ? "0" + String(b) : String(b);
-  let date = new Date(`2016-${month}-${days}`);
-  return yoil[date.getDay()];
+  const date = new Date(`2016-${a.toString().padStart(2, '0')}-${b.toString().padStart(2, '0')}`);
+  return dayOfWeek[date.getDay()];
 }
