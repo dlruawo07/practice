a08bb8118a1492185ae944385ed6b9023530c54d
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 12:25:32 +0900

    added comment

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -8,3 +8,21 @@ function solution(participant, completion) {
     
     return participant[participant.length - 1];
 }
+
+// 기존 코드
+// function solution(participant, completion) {
+//   let obj = {};
+    
+//   participant.forEach((ele) => {
+//       obj[ele] = obj[ele] === undefined ? 1 : obj[ele] + 1;
+//   });
+//   completion.forEach((ele) => { obj[ele]-- });
+
+//   for (let key in obj)
+//     if (obj[key] === 1) return key;
+// }
+
+// 두 배열을 먼저 정렬하고
+// 일치하지 않는 인덱스의 값을 반환
+// 그게 아니라면 마지막 인덱스 반환
+// 시간 복잡도: O(n)
