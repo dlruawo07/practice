a1bf55019f328e703cb3bb422afaa924a1ff1021
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 11:33:28 +0900

    added comment

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,3 +1,5 @@
 function solution(s) {
     return (s.length % 2 === 0) ? s.substring(s.length / 2 - 1, s.length / 2 + 1) : s[Math.floor(s.length / 2)];
 }
+
+// chatGPT 의견: substr은 MDN에서 권장하지 않는 메서드이므로 substring을 권장한다.
