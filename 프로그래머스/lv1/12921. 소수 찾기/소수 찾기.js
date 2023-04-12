f31deccb8a58e55dac3d371872efcf7ac541042d
dlruawo07 <dlruawo07@gmail.com>
2023-04-12 17:03:25 +0900

    refactored (10 times faster with eratosthenes sieve)

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,14 +1,11 @@
-function isPrime(n) {
-    if (Math.sqrt(n) === Math.floor(Math.sqrt(n))) return false;
-    for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
-        if (n % i === 0) return false;
-    }
-    return true;
-}
 function solution(n) {
-    var answer = 0;
-    for (let i = 2; i <= n; i++) {
-        if (isPrime(i)) answer++;
+  let prime = new Array(n + 1).fill(true);
+  prime[0] = false;
+  prime[1] = false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (prime[i]) {
+      for (let j = i * i; j <= n; j += i) prime[j] = false;
     }
-    return answer;
+  }
+  return prime.filter((v) => v).length;
 }
