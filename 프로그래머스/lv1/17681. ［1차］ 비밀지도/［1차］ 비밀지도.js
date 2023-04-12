75474454152e923e5c35064d1725a0185f0b64d1
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 14:23:20 +0900

    refactored

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,15 +1,14 @@
 function solution(n, arr1, arr2) {
   let answer = [];
   let secret1 = arr1
-    .map((ele) => ele.toString(2).replaceAll("0", " ").replaceAll("1", "#").padStart(n, " "));
-
+    .map((ele) => ele.toString(2).padStart(n, "0"));
   let secret2 = arr2
-    .map((ele) => ele.toString(2).replaceAll("0", " ").replaceAll("1", "#").padStart(n, " "));
+    .map((ele) => ele.toString(2).padStart(n, "0"));
 
   for (let i = 0; i < n; i++) {
     let str = "";
     for (let j = 0; j < n; j++) {
-      if (secret1[i][j] === "#" || secret2[i][j] === "#") str += "#";
+      if (secret1[i][j] === '1' || secret2[i][j] === '1') str += "#";
       else str += " ";
     }
     answer.push(str);
