105bce8384c3153147b7506fc1be94fc229b5767
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-12 17:11:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,17 +1,9 @@
 function solution(n, arr1, arr2) {
-  let answer = [];
-  let secret1 = arr1
-    .map((ele) => ele.toString(2).padStart(n, "0"));
-  let secret2 = arr2
-    .map((ele) => ele.toString(2).padStart(n, "0"));
-
+  const answer = [];
   for (let i = 0; i < n; i++) {
-    let str = "";
-    for (let j = 0; j < n; j++) {
-      if (secret1[i][j] === '1' || secret2[i][j] === '1') str += "#";
-      else str += " ";
-    }
-    answer.push(str);
+    const binary = (arr1[i] | arr2[i]).toString(2).padStart(n, '0');
+    const decrypted = binary.replace(/0/g, ' ').replace(/1/g, '#');
+    answer.push(decrypted);
   }
   return answer;
 }
