b60382501e5e9ff34f959a79451216753d5a641d
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-04-13 17:44:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -2,13 +2,11 @@ function solution(id_list, report, k) {
   let reportedUsers = {};
   let reporterReported = {};
 
-  for (let id of id_list) {
-    reporterReported[id] = 0;
-  }
+  for (let id of id_list) reporterReported[id] = 0;
 
   let reportSet = new Set(report);
   for (let one of reportSet) {
-    let [reporter, reported] = one.split(" ");
+    let reported = one.split(" ")[1];
     if (reportedUsers[reported] !== undefined) reportedUsers[reported]++;
     else reportedUsers[reported] = 1;
   }
